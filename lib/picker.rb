e5ae83f991517a2655560558a851f64ebf120057
Joshua Mathews <josh@joshuas-mbp.home>
2018-12-29 16:14:22 -0700

    pick returns error without dictionary

--- lib/picker.rb
+++ lib/picker.rb
@@ -1,3 +1,11 @@
 class Picker
-
+  attr_reader :dictionary
+  def initialize(args)
+    @dictionary = args.fetch(:dictionary, nil)
+  end
+  def pick
+    if !dictionary
+      return "error"
+    end
+  end
 end
--- spec/picker_spec.rb
+++ spec/picker_spec.rb
@@ -1,4 +1,11 @@
 require 'picker'
 describe Picker do
-
+  describe '.pick' do
+    context 'no dictionary' do
+      it "returns error" do
+        temp = Picker.new({})
+        expect(temp.pick).to eql("error")
+      end
+    end
+  end
 end
