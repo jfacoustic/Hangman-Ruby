cb647b0ef786b9f1eb3bfd8896a6605290f3eff9
Joshua Mathews <josh@joshuas-mbp.home>
2018-12-29 18:04:16 -0700

    Finished view

--- lib/view.rb
+++ lib/view.rb
@@ -1,7 +1,14 @@
 class View
-  attr_reader :current, :img_dir
+  attr_reader  :img_dir, :img_header
   def initialize(args)
-    @current = args.fetch(:current, 0)
-    @img_dir = args.fetch(:img_dir, "../resources/")
+    @img_dir = args.fetch(:img_dir, "resources/")
+    @img_header = args.fetch(:img_header, "h")
+  end
+  def getImg x
+    image = ""
+    File.open(img_dir + img_header + x.to_s + ".txt").each do |line|
+      image += line
+    end
+    return image
   end
 end
--- spec/view_spec.rb
+++ spec/view_spec.rb
@@ -1,19 +1,37 @@
 require 'view'
 
+def thisImg(index)
+  image = ""
+  File.open('resources/h' + index.to_s + ".txt").each do |line|
+    image += line
+  end
+  image
+end
+
 describe View do
-  describe ".current" do
+  describe ".img_dir" do
     context "default" do
-      it "should be zero" do
+      it "should be 'resources/'" do
         temp = View.new({})
-        expect(temp.current).to eql(0)
+        expect(temp.img_dir).to eql('resources/')
       end
     end
   end
-  describe ".img_dir" do
+  describe ".img_header" do
     context "default" do
-      it "should be '../resources/'" do
+      it "should be 'h'" do
         temp = View.new({})
-        expect(temp.img_dir).to eql('../resources/')
+        expect(temp.img_header).to eql('h')
+      end
+    end
+  end
+  describe ".getImg" do
+    (0..6).each do |x|
+      context "default" do
+        it "gets #{x}" do
+          temp = View.new({})
+          expect(temp.getImg(x)).to eql(thisImg(x))
+        end
       end
     end
   end
